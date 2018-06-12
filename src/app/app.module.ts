bb2f87a6abe78250303022e4f9c25418abdac393
Arthur Marty <arthurmarty31@gmail.com>
2018-06-12 14:49:44 +0200

    Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserPageComponent,
+    HomeComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
